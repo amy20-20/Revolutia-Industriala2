39b0238bb963dcebcd53eef8a089b02d958e03bc
amy20-20 <amalia.stepan@outlook.com>
2025-03-26 17:08:25 +0200

    Add files via upload

---
+++ aplicatii/question.js
@@ -0,0 +1,52 @@
+const quiz=[
+    {
+        q:'Ce fel de revoluție a fost revoluția industrială?',
+        options:['O revoluție de tip politic.','Un război civil.','O revoluție ce a creat o nouă economie cu caracter industrial și o civilizație specifică.','Niciuna dintre aceste opțiuni.'],
+        answer:2
+    },
+    {
+        q:'Cine a inventat mașina cu aburi?',
+        options:['James Watt','George Stephenson','Thomas Edison','Niciuna dintre aceste opțiuni.'],
+        answer:0
+    },
+    {
+        q:'Care revoluție a fost cunoscută drept  „revoluție tehnologică”?',
+        options:['Prima','A doua','A treia','A patra'],
+        answer:1
+    },
+    {
+        q:'Care dintre aceste invenți/inovați/cercetări nu au aparținut celei de a doua Revoluție Industrială?',
+        options:['Studiul atomului.','Electricitatea.','Descoperirea radiului.','Toate au aparținut.'],
+        answer:3
+    },
+    {
+        q:'Când a fost inventată mașina Turing?',
+        options:['2010','1963','1890','1936'],
+        answer:3
+    },
+    {
+        q:'Care limbaj de programare a fost inventat de Konrad Zuse?',
+        options:['C++','Plankalkül','Pascal','Cobol'],
+        answer:1
+    },
+    {
+        q:'În ce an a apărut World Wide Web?',
+        options:['1983','1990','1973','1980'],
+        answer:1
+    },
+    {
+        q:'Câte generații de calculatoare există până acum?',
+        options:['5','6','4','3'],
+        answer:0
+    },
+    {
+        q:'Care a fost primul calculator electronic programabil?',
+        options:['Felix','Z1','Mașina Turing','ENIAC'],
+        answer:3
+    },
+    {
+        q:'Un limbaj de programare din generația 2 a calculatoarelor?',
+        options:['Fortan','Lisp','Pascal','Prolog'],
+        answer:0
+    },
+]
